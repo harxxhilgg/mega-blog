dea27bc41c6daeb01f7d6bf37d425e4361fc9065
Harshil Patel <harshilp046@gmail.com>
2024-09-29 00:21:51 +0530

    set up appwrite backend-as-a-service id's with react

---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+// importing and exporting environment variables
+
+const conf = {
+  appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectID: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseID: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteCollectionID: String(import.meta.env.VITE_COLLECTION_ID),
+  appwriteBucketID: String(import.meta.env.VITE_BUCKET_ID),
+}
+
+
+export default conf
