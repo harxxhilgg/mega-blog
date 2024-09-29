f9d860757a662d5767aeba2a84db4ff184c5d862
Harshil Patel <harshilp046@gmail.com>
2024-09-30 00:45:32 +0530

    Changes
    1. Added loading state, added effect to get current user-data (to check if user is already logged-in or not, if not then send user to login screen) and then added .finally method to end loading screen.
    2. Used different return method to render components. 3.Added structure of Outlet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logout} from './features/authSlice'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import { Outlet } from 'react-router-dom';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
 
-  return (
-    <>
-      <h1>A blog app with appwrite.</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
